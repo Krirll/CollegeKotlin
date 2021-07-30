55305a9c78a6134ebe1e88d44259d1ae3a354121
Krirll <balalaev20022@gmail.com>
2021-07-31 01:41:54 +0300

    практическая №2 задание №5

---
+++ PracticKotlin2/src/task5.kt
@@ -0,0 +1,33 @@
+/*
+создайте функцию, которая реализует алгоритм второго задания
+первой практической, в которую все числа передаются как аргумент
+функции. Например f(123,25,222)
+*/
+
+fun countSameNumbers(vararg numbers : String) : String {
+    var existsNumbers = ""
+    try {
+        for (sym in numbers[0]) {
+            var currentCount = 0
+            for (nextNum in numbers) {
+                if (nextNum.contains(sym)) currentCount += 1
+            }
+            if (currentCount == numbers.count()) {
+                if (!existsNumbers.contains(sym)) existsNumbers += "$sym "
+            }
+        }
+    }
+    catch(ex : StringIndexOutOfBoundsException) {
+        println("Incorrect string! Was exception")
+        existsNumbers = ""
+    }
+    return if (existsNumbers == "") "nothing" else existsNumbers
+}
+
+fun main() {
+    print("Write numbers with spaces -> ")
+    val numbers = readLine()
+    if (numbers != null) {
+        println("Exists: ${countSameNumbers(*numbers.split(" ").toTypedArray())}")
+    }
+}
