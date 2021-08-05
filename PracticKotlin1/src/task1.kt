afbf32d0f3fa61f840c59c9b84f1da95efb2bb69
Krirll <balalaev20022@gmail.com>
2021-08-06 01:26:56 +0300

    задание №1 - немного подправил, задание №3 - переписал на оптимальный вариант

--- PracticKotlin1/src/task1.kt
+++ PracticKotlin1/src/task1.kt
@@ -3,12 +3,10 @@ fun main() {
     print("Write number (must be > 0) -> ")
     val number = readLine()
     if (number != null) {
-        if (number.toIntOrNull() != null) {
-            var sum = 0
-            for (currentChar in number) {
-                if ((currentChar - '0') % 2 != 0) sum += currentChar - '0'
-            }
-            println("Sum = $sum")
-        } else println("Number must be an integer value and more then 0")
+        var sum = 0
+        for (currentChar in number) {
+            if ((currentChar - '0') % 2 != 0) sum += currentChar - '0'
+        }
+        println("Sum = $sum")
     }
 }
--- PracticKotlin1/src/task3.kt
+++ PracticKotlin1/src/task3.kt
@@ -6,8 +6,26 @@
 
 fun main() {
     print("Write some words with spaces -> ")
-    println(readLine()?.split(" ")
-                      ?.firstOrNull { it.isNotEmpty() && it.length % 2 != 0 }
-                      ?.toString()
-                      ?.last() ?: "empty result")
+    val words = readLine()
+    if (words != null && words.isNotEmpty()) {
+        var flag = false
+        var currentIndex = 0
+        var lengthOfWord = 0
+        var result = ""
+        while (!flag && currentIndex < words.length) {
+            if (words[currentIndex] != ' ') lengthOfWord += 1
+            else {
+                if (lengthOfWord % 2 != 0) {
+                    result += "${words[currentIndex - 2]}"
+                    flag = true
+                }
+                else lengthOfWord = 0
+            }
+            currentIndex += 1
+        }
+        println("result = ${if (result == "") "nothing" else result}")
+    }
+    else {
+        println("Error input string! You must write some words with spaces between them")
+    }
 }
