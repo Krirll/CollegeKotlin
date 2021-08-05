4c83a2ae61f86c8efe68e84e270b5131efdf4263
Krirll <balalaev20022@gmail.com>
2021-08-05 21:48:51 +0300

    сделал задание 1-3 практическойо №3

--- PracticKotlin3/src/taks1_3.kt
+++ PracticKotlin3/src/taks1_3.kt
@@ -1,36 +1,10 @@
 /*
 задания №1-3 практической №2 выполнить с использованием функционального
 подхода в одно выражение каждое (без учета ввода-вывода)
+сумма нечетных цифр
 */
-private fun singleCheck(num : Int) = if (num % 2 != 0) num else 0
-
-private tailrec fun sumTailrec(number : Int, sum : Int = 0): Int = if (number == 0) sum
-else sumTailrec(number / 10, sum + singleCheck(number % 10))
-
-private fun sum(number : Int) : Int {
-    /*var sum = 0
-    var loopNumber = number
-    while (loopNumber != 0) {
-        sum += singleCheck(loopNumber % 10)
-        loopNumber /= 10
-    }*/
-    return number.toString().split(" ").map { it.toInt() }.sumOf {
-        if (it % 2 != 0) it else 0
-    }
-}
 
 fun main() {
     print("Write number (must be > 0) -> ")
-    val number = readLine()
-    if (number != null) {
-        if (number.toIntOrNull() != null) {
-            if (number.toInt() > 0) {
-                var result = sum(number.toInt())
-                println(if (result == 0) "sum = 0 or was error" else "simple function: $result")
-                result = sumTailrec(number.toInt())
-                println(if (result == 0) "sum = 0 or was error" else "tailrec function: $result")
-            } else println("Number must be more than 0")
-        } else println("Number must be an integer value and more than 0")
-    }
-    else print("value is null")
+    println("result: ${readLine()?.toList()?.map { it.code - 48 }?.filter { it % 2 != 0 }?.sum()}")
 }
