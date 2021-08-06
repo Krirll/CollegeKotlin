b931f6d08243517d6be96dc121783c1e30aa085c
Krirll <balalaev20022@gmail.com>
2021-08-06 21:18:01 +0300

    сделал задание №6 практическая №2

--- PracticKotlin2/src/task6.kt
+++ PracticKotlin2/src/task6.kt
@@ -3,22 +3,11 @@
 с параметром типа Int и результатами типа Int возвращает
 новую функцию - произведение данных (кол-во исходных функций - любое)
 */
-
-private fun sum(numbers : List<Int>) : Int {
-    var result = 0
-    numbers.forEach {
-        if (result == 0) result += 1
-        result *= it
-    }
-    return result
+private fun multiply(vararg functions : (Int) -> Int) : (Int) -> Int {
+    return { x -> functions.map { it(x) }.fold(1) {res, x -> res * x} }
 }
 
 fun main() {
-    print("Write some integer values with spaces between them -> ")
-    val data = readLine()
-    if (data != null) {
-        val listOfInt = data.split(" ").mapNotNull { it.toIntOrNull() }
-        println("sum = ${sum(listOfInt)}")
-    }
-    else println("Error! Incorrect input values")
+    val function = multiply({x : Int -> x * x}, { x : Int -> x * x * x})
+    println(function(3))
 }
