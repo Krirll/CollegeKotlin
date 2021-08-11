0fc96af3fc421554f6c53fcf865f00caf9c89b35
Krirll <balalaev20022@gmail.com>
2021-08-12 00:35:21 +0300

    сделал задание №6 пр. №3

---
+++ PracticKotlin3/src/task6.kt
@@ -0,0 +1,14 @@
+/*
+по числу фибоначчи найти его номер
+*/
+
+fun main() {
+    print("Write the fibonacci number -> ")
+    val number = readLine()
+    val result = generateSequence(Pair(1, 1)) {
+                        Pair(it.second, it.first + it.second)
+                    }.takeWhile { it.first <= number?.toInt()!! }.map { it.first }
+                                                                 .toList()
+                                                                 .indexOfFirst { it == number?.toInt() } + 1
+    println("result = ${if (result == 0) "empty result" else result}")
+}
