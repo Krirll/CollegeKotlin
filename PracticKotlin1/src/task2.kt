9fa1cbbc857fda8fe6d8329693c9d8ded333283a
Krirll <balalaev20022@gmail.com>
2021-09-16 22:50:42 +0300

    переписал на более оптимальный вариант

--- PracticKotlin1/src/task2.kt
+++ PracticKotlin1/src/task2.kt
@@ -1,46 +1,35 @@
 //какие цифры присутствуют в каждом неотрицательном числе(разделены одним пробелом)
+//массив из 10 единиц, если встречается цифра то увеличивать элемент массива
 fun main() {
     print("Write numbers with spaces -> ")
-    val numbers = readLine()
-    if (numbers != null && numbers != "") {
-        var currentIndex = -1
+    val string = readLine()
+    if (string != null && string != "") {
+        var currentIndex = 0
+                          //0  1  2  3  4  5  6  7  8  9
+        val array = arrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+        var exist = false
         var countNumbers = 0
-        var loopFlag = false
-        var existsNumbers = ""
-        for (i in numbers.indices) { //считаю количество чисел
-            if (i != numbers.length - 1) {
-                if (numbers[i] != ' ' && numbers[i + 1] == ' ') {
+        for (int in string) {
+            if (!exist && int == string[currentIndex]) {
+                exist = true
+            }
+            if (int == ' ') {
+                var a = 0
+                if (currentIndex != 0)
+                    a = string[currentIndex - 1].code
+                if (a in 0..9) {
+                    array[a] += 1
                     countNumbers += 1
                 }
-            } else if (numbers[i] != ' ') countNumbers += 1
-        }
-        while (!loopFlag) { //основной цикл
-            var count = 0
-            currentIndex += 1
-            if (numbers[currentIndex] != ' ') {
-                var existsFlag = false
-                for (next in currentIndex until numbers.length) { //считаю, в скольки числах присутствует текущая цифра
-                    if (numbers[next] in '0'..'9') {
-                        if (numbers[currentIndex] == numbers[next]) {
-                            if (!existsFlag) {
-                                count += 1
-                                existsFlag = true
-                            }
-                        }
-                    }
-                    else existsFlag = false
-                }
-                if (count == countNumbers) {
-                    var flag = false
-                    for (c in existsNumbers.indices) {
-                        if (existsNumbers[c] == numbers[currentIndex]) flag = true
-                    }
-                    if (!flag) existsNumbers += numbers[currentIndex] + " "
-                }
+                exist = false
             }
-            else loopFlag = true
+            currentIndex += 1
         }
-        println("Exists: ${if (existsNumbers == "") "nothing" else existsNumbers}")
+        currentIndex = 0
+        while (currentIndex < array.size) {
+            if (array[currentIndex] == countNumbers) print("$currentIndex ")
+            currentIndex += 1
+        }
     }
     else println("You must write integer values with spaces!")
 }
