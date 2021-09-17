5fc165b57b0aff8df629bc3c1e16bc2ec01fd6c1
Krirll <balalaev20022@gmail.com>
2021-09-17 14:26:16 +0300

    немного подправил

--- PracticKotlin3/src/task6.kt
+++ PracticKotlin3/src/task6.kt
@@ -5,9 +5,15 @@
 fun main() {
     print("Write the fibonacci number -> ")
     val number = readLine()
-    val result = generateSequence(Pair(1, 1)) {
-                        Pair(it.second, it.first + it.second)
-                    }.takeWhile { it.first <= number?.toInt()!! }.map { it.first }
-                                                                 .indexOfFirst { it == number?.toInt() } + 1
-    println("result = ${if (result == 0) "empty result" else result}")
+    if (number != null && number.isNotEmpty()) {
+        val result =
+            generateSequence(Pair(1, 1)) {
+                Pair(it.second, it.first + it.second)
+            }.takeWhile { it.first <= number.toInt() }
+             .map { it.first }
+             .indexOfFirst { it == number.toInt() } + 1
+        println("result = ${if (result == 0) "empty result" else result}")
+    }
+    else
+        println("empty string!")
 }
