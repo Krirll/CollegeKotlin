6c1da68f747d2d542a38fb72185c01831cb3822e
Krirll <balalaev20022@gmail.com>
2021-09-17 14:24:07 +0300

    исправил на оптимальный вариант

--- PracticKotlin1/src/task3.kt
+++ PracticKotlin1/src/task3.kt
@@ -3,27 +3,52 @@
 символов в строке (в строке указываются только слова
 разделенные одним или несколькими пробелами)
 */
+//держать число в переменной(если нет слова большего по длине чем первое, то первое максимум)
 
+//если есть, то брать следующее лучшее после первого
+//держать в переменной только символ и длину слова
 fun main() {
     print("Write some words with spaces -> ")
     val words = readLine()
     if (words != null && words.isNotEmpty()) {
         var flag = false
         var currentIndex = 0
-        var lengthOfWord = 0
-        var result = ""
+        var currentLengthOfWord = 0
+        var saveLength = 0
+        var nextLengthOfWord = 0
+        var sym = ' '
         while (!flag && currentIndex < words.length) {
-            if (words[currentIndex] != ' ') lengthOfWord += 1
+            if (words[currentIndex] != ' ' && saveLength == 0)
+                currentLengthOfWord += 1
             else {
-                if (lengthOfWord % 2 != 0) {
-                    result += "${words[currentIndex - 2]}"
-                    flag = true
+                if (currentLengthOfWord % 2 != 0) {
+                    if (saveLength == 0) {
+                        sym = words[currentIndex - 1]
+                        saveLength = currentLengthOfWord
+                    }
+                    while (!flag && currentIndex < words.length) {
+                        if (words[currentIndex] == ' ') {
+                            if (nextLengthOfWord >= 1)
+                                flag = true
+                        }
+                        else nextLengthOfWord += 1
+                        if (!flag) currentIndex += 1
+                    }
+                    flag = false
+                    if (nextLengthOfWord % 2 != 0 && nextLengthOfWord > currentLengthOfWord) {
+                        flag = true
+                        sym = words[currentIndex - 1]
+                    }
+                    else nextLengthOfWord = 0
                 }
-                else lengthOfWord = 0
+                else {
+                    currentLengthOfWord = 0
+                    saveLength = 0
+                }
             }
             currentIndex += 1
         }
-        println("result = ${if (result == "") "nothing" else result}")
+        println("result = ${if (sym == ' ') "nothing" else sym}")
     }
     else {
         println("Error input string! You must write some words with spaces between them")
